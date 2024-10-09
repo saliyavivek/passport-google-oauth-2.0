29a1d25ad6d2359c3fea86ed5a676c9b0123f59f
Vivek Saliya <viveksaliya004@gmail.com>
2024-10-09 22:03:37 +0530

    simple google authentication using passport

---
+++ index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+require("./auth");
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+const app = express();
+app.use(session({ secret: "secret" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.send("<a href='/auth/google'>Authenticate with Google</a>");
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/failure",
+  })
+);
+
+app.get("/auth/failure", (req, res) => {
+  res.send("something went wrong!");
+});
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  res.send(`Hello, ${req.user.displayName}`);
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.send("goodbye");
+});
+
+app.listen(5000, () => console.log("listening to port 5000"));
